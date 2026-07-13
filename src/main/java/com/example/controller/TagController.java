3a3bee5941e170bf89bd2e16edea8b2ebca6ad2f
vrma <vrmachado@gmail.com>
2026-07-13 10:47:12 +0200

    Tag Controller en proceso de implementacion

--- src/main/java/com/example/controller/TagController.java
+++ src/main/java/com/example/controller/TagController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,5 +68,40 @@ public class TagController {
 
         List<Tutorial> tutorials = tutorialRepository.findTutorialsByTagsId(tagId);
         return new ResponseEntity<>(tutorials, HttpStatus.OK);
+    }
+
+    @PostMapping("/tutorials/{tutorialId}/tags")
+    public ResponseEntity<Tag> addTag(@PathVariable Long tutorialId,
+            @RequestBody Tag tagRequest) {
+        Tag tag = tutorialRepository.findById(tutorialId).map(tutorial -> {
+            long tagId = tagRequest.getId();
+
+            // tag is existed
+            if (tagId != 0L) {
+                Tag _tag = tagRepository.findById(tagId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Not found Tag with id = "
+                                + tagId));
+                tutorial.addTag(_tag);
+                tutorialRepository.save(tutorial);
+                return _tag;
+            }
+
+            // add and create new Tag
+            tutorial.addTag(tagRequest);
+            return tagRepository.save(tagRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = "
+                + tutorialId));
+
+        return new ResponseEntity<>(tag, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/tags/{id}")
+    public ResponseEntity<Tag> updateTag(@PathVariable long id, @RequestBody Tag tagRequest) {
+        Tag tag = tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("TagId " + id + "not found"));
+
+        tag.setName(tagRequest.getName());
+
+        return new ResponseEntity<>(tagRepository.save(tag), HttpStatus.OK);
     }
 }
