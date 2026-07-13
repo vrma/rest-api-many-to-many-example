812935a5537830b6197218ad9b72d0ef13aabf44
vrma <vrmachado@gmail.com>
2026-07-13 10:28:28 +0200

    TagController en curso

--- src/main/java/com/example/controller/TagController.java
+++ src/main/java/com/example/controller/TagController.java
@@ -1,5 +1,69 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entities.Tag;
+import com.example.entities.Tutorial;
+import com.example.exception.ResourceNotFoundException;
+import com.example.repository.TagRepository;
+import com.example.repository.TutorialRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class TagController {
 
+    private final TutorialRepository tutorialRepository;
+    private final TagRepository tagRepository;
+
+    @GetMapping("/tags")
+    public ResponseEntity<List<Tag>> getAllTags() {
+        List<Tag> tags = new ArrayList<Tag>();
+
+        tagRepository.findAll().forEach(tags::add);
+
+        if (tags.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(tags, HttpStatus.OK);
+    }
+
+    @GetMapping("/tutorials/{tutorialId}/tags")
+    public ResponseEntity<List<Tag>> getAllTagsByTutorialId(@PathVariable Long tutorialId) {
+        if (!tutorialRepository.existsById(tutorialId)) {
+            throw new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId);
+        }
+
+        List<Tag> tags = tagRepository.findTagsByTutorialsId(tutorialId);
+        return new ResponseEntity<>(tags, HttpStatus.OK);
+    }
+
+    @GetMapping("/tags/{id}")
+    public ResponseEntity<Tag> getTagsById(@PathVariable Long id) {
+        Tag tag = tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Tag with id = " + id));
+
+        return new ResponseEntity<>(tag, HttpStatus.OK);
+    }
+
+    @GetMapping("/tags/{tagId}/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorialsByTagId(@PathVariable Long tagId) {
+        if (!tagRepository.existsById(tagId)) {
+            throw new ResourceNotFoundException("Not found Tag  with id = " + tagId);
+        }
+
+        List<Tutorial> tutorials = tutorialRepository.findTutorialsByTagsId(tagId);
+        return new ResponseEntity<>(tutorials, HttpStatus.OK);
+    }
 }
